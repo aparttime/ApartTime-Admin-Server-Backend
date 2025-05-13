8dec7ffa7c9f538745fdb0574c2bc10dd0196a3d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-13 17:30:30 +0900

    initial commit

---
+++ src/main/java/com/aparttime/AparttimeAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.aparttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AparttimeAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AparttimeAdminServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aparttime/AparttimeAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aparttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AparttimeAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
