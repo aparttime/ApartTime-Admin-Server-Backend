47b3ff589c3f82029d318468905c018710e69a86
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 22:57:36 +0900

    feat: implement admin signup page view in `AdminController`

--- src/main/java/com/aparttime/admin/controller/AdminController.java
+++ src/main/java/com/aparttime/admin/controller/AdminController.java
@@ -1,5 +1,24 @@
 package com.aparttime.admin.controller;
 
+import com.aparttime.admin.dto.request.AdminSignupRequest;
+import com.aparttime.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/signup")
+    public String showSignupForm(Model model) {
+        model.addAttribute("adminSignupRequest", new AdminSignupRequest());
+        return "admin/signup";
+    }
 
 }
