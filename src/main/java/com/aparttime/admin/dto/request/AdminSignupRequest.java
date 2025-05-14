4ebf16c75d42eacaca2d8393b0961d54a5884c62
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 22:54:09 +0900

    refactor: convert `AdminSignupRequest` from record to class

--- src/main/java/com/aparttime/admin/dto/request/AdminSignupRequest.java
+++ src/main/java/com/aparttime/admin/dto/request/AdminSignupRequest.java
@@ -1,8 +1,20 @@
 package com.aparttime.admin.dto.request;
 
-public record AdminSignupRequest(
-    String username,
-    String password
-) {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AdminSignupRequest {
+
+    private String username;
+    private String password;
+
+    public AdminSignupRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
 }
