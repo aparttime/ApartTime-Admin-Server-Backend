8ef1505537318680067438b682f4324606fd0b95
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 16:14:02 +0900

    feat: add JPA auditing annotation in `AparttimeAdminServerApplication`

--- src/main/java/com/aparttime/AparttimeAdminServerApplication.java
+++ src/main/java/com/aparttime/AparttimeAdminServerApplication.java
@@ -2,12 +2,14 @@ package com.aparttime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AparttimeAdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AparttimeAdminServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AparttimeAdminServerApplication.class, args);
+    }
 
 }
