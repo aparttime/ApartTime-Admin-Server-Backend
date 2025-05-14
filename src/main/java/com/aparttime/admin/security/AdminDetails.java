343e1a93cc9a4d7e3dda8c66995d7f8c93792d0a
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 21:55:14 +0900

    feat: implement UserDetails in `AdminDetails`

--- src/main/java/com/aparttime/admin/security/AdminDetails.java
+++ src/main/java/com/aparttime/admin/security/AdminDetails.java
@@ -1,5 +1,53 @@
 package com.aparttime.admin.security;
 
-public class AdminDetails {
+import com.aparttime.admin.domain.Admin;
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class AdminDetails implements UserDetails {
+
+    private final Admin admin;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + admin.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return admin.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
