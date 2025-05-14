10c3bf81f1e3003255bb9120d74febf53d6d5951
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 20:27:32 +0900

    refactor: update `Admin` import statement

--- src/main/java/com/aparttime/admin/repository/AdminRepository.java
+++ src/main/java/com/aparttime/admin/repository/AdminRepository.java
@@ -1,6 +1,6 @@
 package com.aparttime.admin.repository;
 
-import com.aparttime.admin.Admin;
+import com.aparttime.admin.domain.Admin;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
