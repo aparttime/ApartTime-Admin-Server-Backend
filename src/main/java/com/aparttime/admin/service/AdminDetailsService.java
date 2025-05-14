38aafeaa0554aea805adfca07dac687f6954cab7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 21:49:10 +0900

    feat: implement loadUserByUsername() in `AdminDetailService`

--- src/main/java/com/aparttime/admin/service/AdminDetailsService.java
+++ src/main/java/com/aparttime/admin/service/AdminDetailsService.java
@@ -1,5 +1,28 @@
 package com.aparttime.admin.service;
 
-public class AdminDetailsService {
+import com.aparttime.admin.domain.Admin;
+import com.aparttime.admin.repository.AdminRepository;
+import com.aparttime.admin.security.AdminDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminDetailsService implements UserDetailsService {
+
+    private final AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(
+        String username
+    ) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("해당 관리자를 찾을 수 없습니다."));
+
+        return new AdminDetails(admin);
+    }
 
 }
