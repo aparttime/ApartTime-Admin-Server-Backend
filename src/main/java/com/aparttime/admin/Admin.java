9384623c047f48b435ee39d09be6e77d1f0f2fdc
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 16:25:53 +0900

    feat: add necessary fields and annotations in `Admin`

--- src/main/java/com/aparttime/admin/Admin.java
+++ src/main/java/com/aparttime/admin/Admin.java
@@ -1,4 +1,41 @@
 package com.aparttime.admin;
 
-public class Admin {
+import com.aparttime.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "admin")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    public Admin(
+        String username,
+        String password
+    ) {
+        this.username = username;
+        this.password = password;
+        this.role = "ADMIN"; // 나중에 상수로 바꿔주기
+    }
 }
