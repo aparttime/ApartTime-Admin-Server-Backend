1189610eb7425379a256f9bb5a64805f45797974
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-14 16:11:53 +0900

    feat: add necessary fields and annotations in `BaseEntity`

--- src/main/java/com/aparttime/common/BaseEntity.java
+++ src/main/java/com/aparttime/common/BaseEntity.java
@@ -1,5 +1,25 @@
 package com.aparttime.common;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 
 }
