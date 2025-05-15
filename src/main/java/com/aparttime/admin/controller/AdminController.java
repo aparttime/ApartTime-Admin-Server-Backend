bbbf8c8304e688a9a5d272171ad88ee17d8cc8af
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-15 20:57:41 +0900

    feat: implement admin signup processing logic in `AdminController`

--- src/main/java/com/aparttime/admin/controller/AdminController.java
+++ src/main/java/com/aparttime/admin/controller/AdminController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,9 +18,19 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/signup")
-    public String showSignupForm(Model model) {
+    public String showSignupForm(
+        Model model
+    ) {
         model.addAttribute("adminSignupRequest", new AdminSignupRequest());
         return "admin/signup";
+    }
+
+    @PostMapping("/signup")
+    public String processSignup(
+        @ModelAttribute AdminSignupRequest adminSignupRequest
+    ) {
+        adminService.signup(adminSignupRequest);
+        return "redirect:/admin/login";
     }
 
 }
