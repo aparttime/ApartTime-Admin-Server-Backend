e4269fd49d9cc1f6df80fb3f315c1dbf0024a84b
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-16 16:20:53 +0900

    feat: implement signup logic in `AuthService`

--- src/main/java/com/aparttime/auth/service/AuthService.java
+++ src/main/java/com/aparttime/auth/service/AuthService.java
@@ -1,5 +1,28 @@
 package com.aparttime.auth.service;
 
+import com.aparttime.admin.domain.Admin;
+import com.aparttime.auth.dto.request.SignupRequest;
+import com.aparttime.admin.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signup(
+        SignupRequest request
+    ) {
+        Admin admin = Admin.of(
+            request.getUsername(),
+            passwordEncoder.encode(request.getPassword())
+        );
+
+        adminRepository.save(admin);
+    }
 
 }
