cca3cfdae163b1e82f724042f5e6c84c08302040
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-17 22:43:22 +0900

    feat: implement admin dashboard view endpoint in `DashboardController`

--- src/main/java/com/aparttime/dashboard/controller/DashboardController.java
+++ src/main/java/com/aparttime/dashboard/controller/DashboardController.java
@@ -1,5 +1,20 @@
 package com.aparttime.dashboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class DashboardController {
+
+    // private final DashboardService dashboardService;
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "dashboard/dashboard";
+    }
 
 }
