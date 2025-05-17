e942ca8b22a595701eb85431912b21c4b46e59d2
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-17 22:42:50 +0900

    feat: add sse controller with `/sse/notification` endpoint in `SseController`

--- src/main/java/com/aparttime/sse/controller/SseController.java
+++ src/main/java/com/aparttime/sse/controller/SseController.java
@@ -1,5 +1,22 @@
 package com.aparttime.sse.controller;
 
+import com.aparttime.sse.service.SseEmitterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sse")
 public class SseController {
+
+    private final SseEmitterService sseEmitterService;
+
+    @GetMapping("/notification")
+    public SseEmitter subscribe() {
+        return sseEmitterService.subscribe();
+    }
 
 }
