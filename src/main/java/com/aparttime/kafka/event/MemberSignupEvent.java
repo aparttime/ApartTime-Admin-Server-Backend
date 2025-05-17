399642116855a2f7d6e15ca393816dfc1e618981
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-17 15:52:06 +0900

    feat: add necessasry fields and annotations in `MemberSignupEvent`

--- src/main/java/com/aparttime/kafka/event/MemberSignupEvent.java
+++ src/main/java/com/aparttime/kafka/event/MemberSignupEvent.java
@@ -1,5 +1,15 @@
 package com.aparttime.kafka.event;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberSignupEvent {
+
+    private Long id;
+    private String username;
+    private String email;
 
 }
