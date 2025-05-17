13850e90913a219c29bbbe557d616bf90ccbc9cf
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-17 22:33:27 +0900

    refactor: change default constructor to public in `SignupRequest`

--- src/main/java/com/aparttime/auth/dto/request/SignupRequest.java
+++ src/main/java/com/aparttime/auth/dto/request/SignupRequest.java
@@ -1,13 +1,12 @@
 package com.aparttime.auth.dto.request;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class SignupRequest {
 
     private String username;
