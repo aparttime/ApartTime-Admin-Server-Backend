87f7e4bf0bab71913d8e7c57c7b3b19c0db162f8
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-19 21:51:26 +0900

    feat: display pending signup count in `DashboardController`

--- src/main/java/com/aparttime/dashboard/controller/DashboardController.java
+++ src/main/java/com/aparttime/dashboard/controller/DashboardController.java
@@ -1,7 +1,9 @@
 package com.aparttime.dashboard.controller;
 
+import com.aparttime.dashboard.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class DashboardController {
 
-    // private final DashboardService dashboardService;
+    private final DashboardService dashboardService;
 
     @GetMapping("/dashboard")
-    public String showDashboard() {
+    public String showDashboard(
+        Model model
+    ) {
+        long pendingCount = dashboardService.getPendingSignupCount();
+        model.addAttribute("pendingCount", pendingCount);
         return "dashboard/dashboard";
     }
 
