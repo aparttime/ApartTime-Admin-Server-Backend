a2d28898a47c7326ab1b501823204cbc900610b7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-20 22:24:40 +0900

    feat: add JPA annotations and fields in `Member`

--- src/main/java/com/aparttime/member/entity/Member.java
+++ src/main/java/com/aparttime/member/entity/Member.java
@@ -1,5 +1,31 @@
 package com.aparttime.member.entity;
 
-public class Member {
+import com.aparttime.common.BaseEntity;
+import com.aparttime.member.MemberStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus status;
 
 }
