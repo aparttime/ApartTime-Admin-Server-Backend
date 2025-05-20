d12a2903edc8503cd0d94e60021656a87882bb7e
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-20 22:23:55 +0900

    feat: add countPendingMembers() in `MemberService`

--- src/main/java/com/aparttime/member/service/MemberService.java
+++ src/main/java/com/aparttime/member/service/MemberService.java
@@ -1,5 +1,18 @@
 package com.aparttime.member.service;
 
+import com.aparttime.member.MemberStatus;
+import com.aparttime.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public long countPendingMembers() {
+        return memberRepository.countByStatus(MemberStatus.PENDING);
+    }
 
 }
