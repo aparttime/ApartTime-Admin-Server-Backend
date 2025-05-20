2e682b66ef112d75aa19afbd2b2cd4aab2d31e66
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-20 22:25:31 +0900

    feat: add getPendingSignupCount() in `DashboardService`

--- src/main/java/com/aparttime/dashboard/service/DashboardService.java
+++ src/main/java/com/aparttime/dashboard/service/DashboardService.java
@@ -1,5 +1,17 @@
 package com.aparttime.dashboard.service;
 
+import com.aparttime.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DashboardService {
+
+    private final MemberService memberService;
+
+    public long getPendingSignupCount() {
+        return memberService.countPendingMembers();
+    }
 
 }
