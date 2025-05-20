9c17ae71605446a615439c745c5e2b6f2f43d08f
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-20 22:19:28 +0900

    feat: implement countByStatus() in `MemberRepository`

--- src/main/java/com/aparttime/member/repository/MemberRepository.java
+++ src/main/java/com/aparttime/member/repository/MemberRepository.java
@@ -1,5 +1,11 @@
 package com.aparttime.member.repository;
 
-public interface MemberRepository {
+import com.aparttime.member.MemberStatus;
+import com.aparttime.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    long countByStatus(MemberStatus status);
 
 }
