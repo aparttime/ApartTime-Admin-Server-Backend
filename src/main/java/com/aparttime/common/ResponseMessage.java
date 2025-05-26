3690c74b97d77a9f067491785db77173d6dff0a6
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-26 17:36:52 +0900

    feat: add enum constants in `ResponseMessage`

--- src/main/java/com/aparttime/common/ResponseMessage.java
+++ src/main/java/com/aparttime/common/ResponseMessage.java
@@ -1,4 +1,16 @@
 package com.aparttime.common;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ResponseMessage {
+
+    // Auth
+    LOGIN_SUCCESS("로그인 성공"),
+    SIGNUP_SUCCESS("회원가입 성공");
+
+    private final String message;
+
 }
