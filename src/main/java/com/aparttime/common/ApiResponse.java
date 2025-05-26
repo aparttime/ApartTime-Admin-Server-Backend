25e8faa8d1a28c34de57e02e9ed74817a12f32f1
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-26 17:35:35 +0900

    feat: add static factory methods in `ApiResponse`

--- src/main/java/com/aparttime/common/ApiResponse.java
+++ src/main/java/com/aparttime/common/ApiResponse.java
@@ -1,4 +1,49 @@
 package com.aparttime.common;
 
-public record ApiResponse() {
+import org.springframework.http.HttpStatus;
+
+public record ApiResponse<T>(
+    int code,
+    String status,
+    String message,
+    T data
+) {
+
+    public static <T> ApiResponse<T> of(
+        HttpStatus status,
+        ResponseMessage message,
+        T data
+    ) {
+        return new ApiResponse<>(
+            status.value(),
+            status.getReasonPhrase(),
+            message.getMessage(),
+            data
+        );
+    }
+
+    public static <T> ApiResponse<T> ok(
+        ResponseMessage message,
+        T data
+    ) {
+        return new ApiResponse<>(
+            HttpStatus.OK.value(),
+            HttpStatus.OK.getReasonPhrase(),
+            message.getMessage(),
+            data
+        );
+    }
+
+    public static <T> ApiResponse<T> created(
+        ResponseMessage message,
+        T data
+    ) {
+        return new ApiResponse<>(
+            HttpStatus.CREATED.value(),
+            HttpStatus.CREATED.getReasonPhrase(),
+            message.getMessage(),
+            data
+        );
+    }
+
 }
