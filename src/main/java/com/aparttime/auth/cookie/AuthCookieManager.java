4e10b24b15485c7b068f0827d43757b18a5d7971
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-27 22:04:13 +0900

    feat: implement refresh token cookie handling in `AuthCookieManager`

--- src/main/java/com/aparttime/auth/cookie/AuthCookieManager.java
+++ src/main/java/com/aparttime/auth/cookie/AuthCookieManager.java
@@ -1,5 +1,39 @@
 package com.aparttime.auth.cookie;
 
+import static com.aparttime.common.constants.CookieConstants.*;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthCookieManager {
+
+    public void addRefreshToken(
+        HttpServletResponse response,
+        String refreshToken
+    ) {
+        Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+
+        cookie.setHttpOnly(REFRESH_TOKEN_COOKIE_HTTP_ONLY);
+        cookie.setSecure(REFRESH_TOKEN_COOKIE_SECURE); // HTTPS 환경에서만 전송
+        cookie.setPath(REFRESH_TOKEN_COOKIE_PATH);
+        cookie.setMaxAge(REFRESH_TOKEN_COOKIE_EXPIRATION_SECONDS);
+
+        response.addCookie(cookie);
+    }
+
+    public void removeRefreshToken(
+        HttpServletResponse response
+    ) {
+        Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, null);
+
+        cookie.setHttpOnly(REFRESH_TOKEN_COOKIE_HTTP_ONLY);
+        cookie.setSecure(REFRESH_TOKEN_COOKIE_SECURE); // HTTPS 환경에서만 전송
+        cookie.setPath(REFRESH_TOKEN_COOKIE_PATH);
+        cookie.setMaxAge(REFRESH_TOKEN_COOKIE_INSTANT_EXPIRE);
+
+        response.addCookie(cookie);
+    }
 
 }
