5fe39160d4d9a38966dd7acc14dba11746c3f564
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-28 18:20:44 +0900

    feat: implement refresh token handling logic in `RefreshTokenRepository`

--- src/main/java/com/aparttime/redis/repository/RefreshTokenRepository.java
+++ src/main/java/com/aparttime/redis/repository/RefreshTokenRepository.java
@@ -1,5 +1,38 @@
 package com.aparttime.redis.repository;
 
+import static com.aparttime.common.constants.RedisConstants.*;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class RefreshTokenRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void save(
+        Long memberId,
+        String refreshToken,
+        long expiration
+    ) {
+        String key = REDIS_REFRESH_TOKEN_PREFIX + memberId;
+        Duration ttl = Duration.ofMillis(expiration);
+        redisTemplate.opsForValue().set(key, refreshToken, ttl);
+    }
+
+    public String get(
+        Long memberId
+    ) {
+        return redisTemplate.opsForValue().get(REDIS_REFRESH_TOKEN_PREFIX + memberId);
+    }
+
+    public void delete(
+        Long memberId
+    ) {
+        redisTemplate.delete(REDIS_REFRESH_TOKEN_PREFIX + memberId);
+    }
 
 }
