2281b9396438629f431afbd3045bb89f699b2a23
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-28 20:25:15 +0900

    feat: add JWT configuration in `JwtProperties`

--- src/main/java/com/aparttime/config/properties/JwtProperties.java
+++ src/main/java/com/aparttime/config/properties/JwtProperties.java
@@ -1,5 +1,16 @@
 package com.aparttime.config.properties;
 
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+
+    private String secret;
+    private long accessTokenExpiration;
+    private long refreshTokenExpiration;
 
 }
