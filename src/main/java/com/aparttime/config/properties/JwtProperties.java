7d1ea7121ad2cd6c77a9b65adf07d83a114aa084
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-29 01:49:42 +0900

    feat: add necessary annotation in `JwtProperties`

--- src/main/java/com/aparttime/config/properties/JwtProperties.java
+++ src/main/java/com/aparttime/config/properties/JwtProperties.java
@@ -1,10 +1,12 @@
 package com.aparttime.config.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
