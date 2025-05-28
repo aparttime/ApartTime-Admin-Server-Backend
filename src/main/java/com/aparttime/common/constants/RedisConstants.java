9fef377cadd464f3ee43913d40582c22f253b563
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-28 18:14:44 +0900

    feat: add constant in `RedisConstants`

--- src/main/java/com/aparttime/common/constants/RedisConstants.java
+++ src/main/java/com/aparttime/common/constants/RedisConstants.java
@@ -1,5 +1,12 @@
 package com.aparttime.common.constants;
 
-public class RedisConstants {
+public final class RedisConstants {
+
+    private RedisConstants() {
+        throw new UnsupportedOperationException("상수 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
+    // refresh token prefix
+    public static final String REDIS_REFRESH_TOKEN_PREFIX = "refresh:";
 
 }
