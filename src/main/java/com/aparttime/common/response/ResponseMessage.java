b4d281aec574cc18124529c757baa7f641c341b5
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-28 09:24:47 +0900

    feat: add enum constant in `ResponseMessage`

--- src/main/java/com/aparttime/common/response/ResponseMessage.java
+++ src/main/java/com/aparttime/common/response/ResponseMessage.java
@@ -9,7 +9,8 @@ public enum ResponseMessage {
 
     // Auth
     LOGIN_SUCCESS("로그인 성공"),
-    SIGNUP_SUCCESS("회원가입 성공");
+    SIGNUP_SUCCESS("회원가입 성공"),
+    LOGOUT_SUCCESS("로그아웃 성공");
 
     private final String message;
 
