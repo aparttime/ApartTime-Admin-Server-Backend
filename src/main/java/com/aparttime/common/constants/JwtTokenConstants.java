9c7e39f413138044bdbf1dce4d47ba0c584f2751
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-29 21:36:03 +0900

    feat: add constants for JWT token in `JwtTokenConstants`

--- src/main/java/com/aparttime/common/constants/JwtTokenConstants.java
+++ src/main/java/com/aparttime/common/constants/JwtTokenConstants.java
@@ -1,6 +1,18 @@
 package com.aparttime.common.constants;
 
-public class JwtTokenConstants {
+public final class JwtTokenConstants {
 
+    private JwtTokenConstants() {
+        throw new UnsupportedOperationException("상수 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
+    // HTTP Authorization 헤더 이름
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // Bearer prefix
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    // Bearer 문자열 길이
+    public static final int BEARER_PREFIX_LENGTH = BEARER_PREFIX.length();
 
 }
