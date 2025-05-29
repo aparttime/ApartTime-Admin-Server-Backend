715ecbcb7d1b47d02536885934535978f82a29af
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-29 21:20:05 +0900

    feaat: add custom exception support in `RestApiException`

--- src/main/java/com/aparttime/exception/RestApiException.java
+++ src/main/java/com/aparttime/exception/RestApiException.java
@@ -1,5 +1,18 @@
 package com.aparttime.exception;
 
-public class RestApiException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class RestApiException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public RestApiException(
+        ErrorCode errorCode
+    ) {
+        super(errorCode.getMessage());
+        this.status = errorCode.getStatus();
+    }
 
 }
