7d35651c533cb6108b522adf06b2ce9c5fe85e4f
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-29 20:42:31 +0900

    feat: implement static factory methods in `ErrorResponse`

--- src/main/java/com/aparttime/exception/dto/ErrorResponse.java
+++ src/main/java/com/aparttime/exception/dto/ErrorResponse.java
@@ -1,5 +1,35 @@
 package com.aparttime.exception.dto;
 
-public record ErrorResponse() {
+import java.util.List;
+import org.springframework.http.HttpStatus;
+
+public record ErrorResponse(
+    int code,
+    String status,
+    List<ErrorDetail> errors
+) {
+
+    public static ErrorResponse of(
+        HttpStatus status,
+        List<ErrorDetail> errors
+    ) {
+        return new ErrorResponse(
+            status.value(),
+            status.getReasonPhrase(),
+            errors
+        );
+    }
+
+    public static ErrorResponse of(
+        HttpStatus status,
+        String type,
+        String message
+    ) {
+        return new ErrorResponse(
+            status.value(),
+            status.getReasonPhrase(),
+            List.of(new ErrorDetail(type, message))
+        );
+    }
 
 }
