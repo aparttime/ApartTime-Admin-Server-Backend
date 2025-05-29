01d10814e1bf7a586dfdaccc2f0e91aaff5bed53
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-29 20:48:53 +0900

    feat: implement `RestApiException` handling logic in `GlobalExceptionHandler`

--- src/main/java/com/aparttime/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aparttime/exception/GlobalExceptionHandler.java
@@ -1,5 +1,26 @@
 package com.aparttime.exception;
 
+import com.aparttime.exception.dto.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RestApiException.class)
+    public ResponseEntity<ErrorResponse> handleRestApiException(
+        RestApiException exception
+    ) {
+        ErrorResponse errorResponse = ErrorResponse.of(
+            exception.getStatus(),
+            exception.getClass().getSimpleName(),
+            exception.getMessage()
+        );
+
+        return ResponseEntity
+            .status(exception.getStatus())
+            .body(errorResponse);
+    }
 
 }
