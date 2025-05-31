092080678849941bef00d12b0f23149af9ef13a0
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-05-31 22:14:28 +0900

    feat: add JWT exception handling logic in `JwtExceptionFilter`

--- src/main/java/com/aparttime/security/filter/JwtExceptionFilter.java
+++ src/main/java/com/aparttime/security/filter/JwtExceptionFilter.java
@@ -1,5 +1,39 @@
 package com.aparttime.security.filter;
 
-public class JwtExceptionFilter {
+import com.aparttime.exception.RestApiException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final HandlerExceptionResolver resolver;
+
+    public JwtExceptionFilter(
+        @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
+    ) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (RestApiException exception) {
+            resolver.resolveException(request, response, null, exception);
+        }
+
+    }
 }
