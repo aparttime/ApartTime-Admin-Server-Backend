b2f13d73c8537934dac384530a9173c5582d6174
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-02 23:57:49 +0900

    feat: add static factory method in `ReissueResult`

--- src/main/java/com/aparttime/auth/dto/result/ReissueResult.java
+++ src/main/java/com/aparttime/auth/dto/result/ReissueResult.java
@@ -1,5 +1,20 @@
 package com.aparttime.auth.dto.result;
 
-public record ReissueResult() {
+import com.aparttime.auth.dto.response.ReissueResponse;
+
+public record ReissueResult(
+    ReissueResponse reissueResponse,
+    String refreshToken
+) {
+
+    public static ReissueResult of(
+        ReissueResponse reissueResponse,
+        String refreshToken
+    ) {
+        return new ReissueResult(
+            reissueResponse,
+            refreshToken
+        );
+    }
 
 }
