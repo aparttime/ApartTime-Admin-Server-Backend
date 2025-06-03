cc28aa3e6a8d73ecf9f5b0640e274b1fd7462898
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-03 18:48:17 +0900

    feat: add default constructor in `EmptyAccessTokenException`

--- src/main/java/com/aparttime/exception/jwt/EmptyAccessTokenException.java
+++ src/main/java/com/aparttime/exception/jwt/EmptyAccessTokenException.java
@@ -1,5 +1,12 @@
 package com.aparttime.exception.jwt;
 
-public class EmptyAccessTokenException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class EmptyAccessTokenException extends RestApiException {
+
+    public EmptyAccessTokenException() {
+        super(EMPTY_ACCESS_TOKEN);
+    }
 }
