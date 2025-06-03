ca841dee4f017d3564e7af516042b5f3747ca2cc
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-03 18:44:26 +0900

    feat: add entry point for handling empty access token exception in `JwtAuthenticationEntryPoint`

--- src/main/java/com/aparttime/security/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/aparttime/security/handler/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,39 @@
 package com.aparttime.security.handler;
 
-public class JwtAuthenticationEntryPoint {
+import com.aparttime.exception.jwt.EmptyAccessTokenException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final HandlerExceptionResolver resolver;
+
+    public JwtAuthenticationEntryPoint(
+        @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
+    ) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void commence(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException authException
+    ) throws IOException, ServletException {
+
+        log.info(">>> JwtAuthenticationEntryPoint URI: {}", request.getRequestURI());
+
+        resolver.resolveException(request, response, null, new EmptyAccessTokenException());
+
+    }
 }
