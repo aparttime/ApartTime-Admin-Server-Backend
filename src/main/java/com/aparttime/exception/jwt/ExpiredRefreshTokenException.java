6e447927aeb88b59388ae2e3b48d12159c44baa3
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 16:03:08 +0900

    feat: add default constructor in `ExpiredRefreshTokenException`

--- src/main/java/com/aparttime/exception/jwt/ExpiredRefreshTokenException.java
+++ src/main/java/com/aparttime/exception/jwt/ExpiredRefreshTokenException.java
@@ -1,5 +1,12 @@
 package com.aparttime.exception.jwt;
 
-public class ExpiredRefreshTokenException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class ExpiredRefreshTokenException extends RestApiException {
+
+    public ExpiredRefreshTokenException() {
+        super(REFRESH_TOKEN_EXPIRED);
+    }
 }
