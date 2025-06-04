2485a27fd13290b55b967b351815845a1e72a699
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 16:21:05 +0900

    feat: add default constructor in `RefreshTokenNotFoundException`

--- src/main/java/com/aparttime/exception/jwt/RefreshTokenNotFoundException.java
+++ src/main/java/com/aparttime/exception/jwt/RefreshTokenNotFoundException.java
@@ -1,5 +1,12 @@
 package com.aparttime.exception.jwt;
 
-public class RefreshTokenNotFoundException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class RefreshTokenNotFoundException extends RestApiException {
+
+    public RefreshTokenNotFoundException() {
+        super(REFRESH_TOKEN_NOT_FOUND);
+    }
 }
