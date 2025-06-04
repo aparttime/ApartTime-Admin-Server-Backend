14595f64dce345d06db7864b6a950c0d04be96da
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 21:41:02 +0900

    chore: remove logging in `JwtAuthenticationEntryPoint`

--- src/main/java/com/aparttime/security/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/aparttime/security/handler/JwtAuthenticationEntryPoint.java
@@ -5,14 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -30,8 +28,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         HttpServletResponse response,
         AuthenticationException authException
     ) throws IOException, ServletException {
-
-        log.info(">>> JwtAuthenticationEntryPoint URI: {}", request.getRequestURI());
 
         resolver.resolveException(request, response, null, new EmptyAccessTokenException());
 
