37ffd6b76142bdbcfe4f39b97e7da75ee4246414
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 11:46:55 +0900

    feat: add default constructor in `EmptyRefreshTokenException`

--- src/main/java/com/aparttime/exception/jwt/EmptyRefreshTokenException.java
+++ src/main/java/com/aparttime/exception/jwt/EmptyRefreshTokenException.java
@@ -1,5 +1,12 @@
 package com.aparttime.exception.jwt;
 
-public class EmptyRefreshTokenException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class EmptyRefreshTokenException extends RestApiException {
+
+    public EmptyRefreshTokenException() {
+        super(EMPTY_REFRESH_TOKEN);
+    }
 }
