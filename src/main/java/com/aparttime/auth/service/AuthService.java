f4d3c5eb0a59b9b7ac5c4eb3e86c3eae638d4186
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 21:44:42 +0900

    feat: implement refresh token reissue and update flow in `AuthService`

--- src/main/java/com/aparttime/auth/service/AuthService.java
+++ src/main/java/com/aparttime/auth/service/AuthService.java
@@ -1,13 +1,17 @@
 package com.aparttime.auth.service;
 
 import com.aparttime.admin.domain.Admin;
+import com.aparttime.auth.dto.response.ReissueResponse;
 import com.aparttime.auth.dto.result.LoginResult;
 import com.aparttime.auth.dto.request.LoginRequest;
 import com.aparttime.auth.dto.request.SignupRequest;
 import com.aparttime.admin.repository.AdminRepository;
 import com.aparttime.auth.dto.response.LoginResponse;
+import com.aparttime.auth.dto.result.ReissueResult;
 import com.aparttime.config.properties.JwtProperties;
 import com.aparttime.exception.auth.InvalidPasswordException;
+import com.aparttime.exception.jwt.EmptyRefreshTokenException;
+import com.aparttime.exception.jwt.RefreshTokenNotFoundException;
 import com.aparttime.exception.member.MemberNotFoundException;
 import com.aparttime.redis.repository.RefreshTokenRepository;
 import com.aparttime.jwt.JwtTokenProvider;
@@ -46,8 +50,8 @@ public class AuthService {
             throw new InvalidPasswordException();
         }
 
-        String accessToken = jwtTokenProvider.createAccessToken(admin.getUsername());
-        String refreshToken = jwtTokenProvider.createRefreshToken(admin.getUsername());
+        String accessToken = jwtTokenProvider.createAccessToken(admin.getId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(admin.getId());
 
         // save refresh token in Redis
         refreshTokenRepository.save(
@@ -66,9 +70,61 @@ public class AuthService {
     }
 
     public void logout(
-        Long memberId
+        Long memberId,
+        String refreshToken
     ) {
-        refreshTokenRepository.delete(memberId);
+        if (refreshToken == null) {
+            return;
+        }
+
+        Long findMemberId = refreshTokenRepository.findMemberIdByRefreshToken(refreshToken);
+
+        if (memberId.equals(findMemberId)) {
+            refreshTokenRepository.deleteByRefreshToken(refreshToken);
+        }
+
+    }
+
+    public ReissueResult reissue(
+        String refreshToken
+    ) {
+
+        if (refreshToken == null) {
+            throw new EmptyRefreshTokenException();
+        }
+
+        jwtTokenProvider.validateRefreshToken(refreshToken);
+
+        Long memberId = refreshTokenRepository.findMemberIdByRefreshToken(refreshToken);
+
+        if (memberId == null) {
+            throw new RefreshTokenNotFoundException();
+        }
+
+        Admin admin = adminRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+
+        refreshTokenRepository.deleteByRefreshToken(refreshToken);
+
+        // 재발급 로직
+        // TODO: 나중에 이 부분 별개의 메서드로 리팩터링 필요 (로그인 로직에서도 재사용)
+
+        String newAccessToken = jwtTokenProvider.createAccessToken(memberId);
+        String newRefreshToken = jwtTokenProvider.createRefreshToken(memberId);
+
+        refreshTokenRepository.save(
+            admin.getId(),
+            newRefreshToken,
+            jwtProperties.getRefreshTokenExpiration()
+        );
+
+        ReissueResponse reissueResponse = ReissueResponse.of(
+            admin.getId(),
+            admin.getUsername(),
+            newAccessToken
+        );
+
+        return ReissueResult.of(reissueResponse, newRefreshToken);
     }
 
 }
