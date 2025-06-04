22300dbb36bd778b73631f46122f8824b90927fc
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 15:52:39 +0900

    feat: add necessary fields in `ReissueResponse`

--- src/main/java/com/aparttime/auth/dto/response/ReissueResponse.java
+++ src/main/java/com/aparttime/auth/dto/response/ReissueResponse.java
@@ -1,5 +1,9 @@
 package com.aparttime.auth.dto.response;
 
-public record ReissueResponse() {
+public record ReissueResponse(
+    Long memberId,
+    String username,
+    String accessToken
+) {
 
 }
