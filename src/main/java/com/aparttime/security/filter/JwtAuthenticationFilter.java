bf055f88950f184cf31215f74a89d2b791364e90
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-04 21:36:00 +0900

    refactor: remove @Component in `JwtAuthenticationFilter`

--- src/main/java/com/aparttime/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/aparttime/security/filter/JwtAuthenticationFilter.java
@@ -12,10 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
