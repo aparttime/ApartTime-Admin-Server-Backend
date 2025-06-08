4718c5f0f528fe210e52d882a8460e34688328b2
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-08 21:08:37 +0900

    feat: add default constructor in `InvalidTokenTypeException`

--- src/main/java/com/aparttime/exception/jwt/InvalidTokenTypeException.java
+++ src/main/java/com/aparttime/exception/jwt/InvalidTokenTypeException.java
@@ -1,5 +1,12 @@
 package com.aparttime.exception.jwt;
 
-public class InvalidTokenTypeException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class InvalidTokenTypeException extends RestApiException {
+
+    public InvalidTokenTypeException() {
+        super(INVALID_TOKEN_TYPE);
+    }
 }
