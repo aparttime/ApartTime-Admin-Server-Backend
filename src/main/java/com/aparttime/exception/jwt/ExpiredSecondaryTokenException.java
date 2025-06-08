70f182ac37803221d295faef65bc81d70da0f946
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-08 21:38:09 +0900

    feat: add default constructor in `ExpiredSecondaryTokenException`

--- src/main/java/com/aparttime/exception/jwt/ExpiredSecondaryTokenException.java
+++ src/main/java/com/aparttime/exception/jwt/ExpiredSecondaryTokenException.java
@@ -1,5 +1,13 @@
 package com.aparttime.exception.jwt;
 
-public class ExpiredSecondaryTokenException {
+import static com.aparttime.exception.ErrorCode.*;
 
+import com.aparttime.exception.RestApiException;
+
+public class ExpiredSecondaryTokenException extends RestApiException {
+
+    public ExpiredSecondaryTokenException() {
+        super(SECONDARY_TOKEN_EXPIRED);
+
+    }
 }
