1de2867cb141fb9c22c2a46a6b9e9efadfb9c5d7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-11 15:46:46 +0900

    feat: add necessary fields in `NotificationMessage`

--- src/main/java/com/aparttime/notification/dto/NotificationMessage.java
+++ src/main/java/com/aparttime/notification/dto/NotificationMessage.java
@@ -1,5 +1,13 @@
 package com.aparttime.notification.dto;
 
-public record NotificationMessage() {
+import java.util.Map;
+
+public record NotificationMessage(
+    String type,
+    String title,
+    String content,
+    String redirectUrl,
+    Map<String, Object> data
+) {
 
 }
