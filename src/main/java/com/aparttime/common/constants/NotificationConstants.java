257da386bb87232a235702e5cb49818f84304878
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-11 16:17:56 +0900

    feat: add constants in `NotificationConstants`

--- src/main/java/com/aparttime/common/constants/NotificationConstants.java
+++ src/main/java/com/aparttime/common/constants/NotificationConstants.java
@@ -1,5 +1,14 @@
 package com.aparttime.common.constants;
 
-public class NotificationConstants {
+public final class NotificationConstants {
+
+    private NotificationConstants() {
+        throw new UnsupportedOperationException("상수 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
+    // 신규 회원가입
+    public static final String MEMBER_SIGNUP_TITLE = "신규 회원가입";
+    public static final String MEMBER_SIGNUP_PREFIX = "신규 회원가입 요청 - ";
+    public static final String MEMBER_SIGNUP_REDIRECT = "/members/signup/pending";
 
 }
