26d5b40cb98dc45132106ade42ccb0bbd45c428d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-11 17:08:46 +0900

    feat: implement @NotificationPolicy annotation

--- src/main/java/com/aparttime/annotation/NotificationPolicy.java
+++ src/main/java/com/aparttime/annotation/NotificationPolicy.java
@@ -1,5 +1,16 @@
 package com.aparttime.annotation;
 
-public class NotificationPolicy {
+import com.aparttime.notification.NotificationType;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotificationPolicy {
+
+    NotificationType value();
 
 }
