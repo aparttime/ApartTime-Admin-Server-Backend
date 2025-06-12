9f0d8dd97fe2d093b20a3345f5255bbc7588c7de
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-12 14:58:06 +0900

    feat: add static factory method in `NotificationMessage`

--- src/main/java/com/aparttime/notification/dto/NotificationMessage.java
+++ src/main/java/com/aparttime/notification/dto/NotificationMessage.java
@@ -1,5 +1,6 @@
 package com.aparttime.notification.dto;
 
+import com.aparttime.notification.NotificationType;
 import java.util.Map;
 
 public record NotificationMessage(
@@ -9,5 +10,19 @@ public record NotificationMessage(
     String redirectUrl,
     Map<String, Object> data
 ) {
+
+    public static NotificationMessage of(
+        NotificationType type,
+        String content,
+        Map<String, Object> data
+    ) {
+        return new NotificationMessage(
+            type.name(),
+            type.getTitle(),
+            content,
+            type.getRedirectUrl(),
+            data
+        );
+    }
 
 }
