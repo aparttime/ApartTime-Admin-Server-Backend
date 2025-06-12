2338f1ac9f4ba993d9903d046dd83107f318ce30
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-12 14:55:35 +0900

    feat: add assembler for signup notification assembly in `MemberSignupNotificationAssembler`

--- src/main/java/com/aparttime/notification/assembler/MemberSignupNotificationAssembler.java
+++ src/main/java/com/aparttime/notification/assembler/MemberSignupNotificationAssembler.java
@@ -1,4 +1,30 @@
 package com.aparttime.notification.assembler;
 
-public class MemberSignupNotificationAssembler {
+import static com.aparttime.common.constants.NotificationConstants.*;
+
+import com.aparttime.annotation.NotificationPolicy;
+import com.aparttime.notification.NotificationType;
+import com.aparttime.notification.dto.NotificationMessage;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+@NotificationPolicy(NotificationType.MEMBER_SIGNUP)
+public class MemberSignupNotificationAssembler implements NotificationAssembler {
+
+    @Override
+    public NotificationMessage assemble(
+        Map<String, Object> data
+    ) {
+
+        String username = data.get(USERNAME).toString();
+        String content = MEMBER_SIGNUP_PREFIX + username;
+
+        return NotificationMessage.of(
+            NotificationType.MEMBER_SIGNUP,
+            content,
+            Map.of(USERNAME, username)
+        );
+
+    }
 }
