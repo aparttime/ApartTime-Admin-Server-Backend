b55841f316fffd25644e922ddcf6e74ce306450a
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-12 14:59:27 +0900

    feat: define notification type with metadata in `NotificationType`

--- src/main/java/com/aparttime/notification/NotificationType.java
+++ src/main/java/com/aparttime/notification/NotificationType.java
@@ -1,4 +1,25 @@
 package com.aparttime.notification;
 
+import static com.aparttime.common.constants.NotificationConstants.*;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum NotificationType {
+
+    MEMBER_SIGNUP(
+        "신규 회원가입",
+        "신규 회원가입 요청 - ",
+        "/members/signup/pending",
+        BASE_TOPIC + "/signup"
+    );
+
+
+    private final String title;
+    private final String contentPrefix;
+    private final String redirectUrl;
+    private final String destination;
+
 }
