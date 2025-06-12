545bddc00f0f19aa201f16daec30c0ecf2c86668
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-12 14:49:26 +0900

    refactor: separate JWT create and validate logic by token type in `JwtTokenProvider`

--- src/main/java/com/aparttime/jwt/JwtTokenProvider.java
+++ src/main/java/com/aparttime/jwt/JwtTokenProvider.java
@@ -1,12 +1,17 @@
 package com.aparttime.jwt;
 
+import static com.aparttime.common.constants.JwtTokenConstants.*;
+
 import com.aparttime.config.properties.JwtProperties;
 import com.aparttime.exception.jwt.ExpiredAccessTokenException;
 import com.aparttime.exception.jwt.ExpiredRefreshTokenException;
+import com.aparttime.exception.jwt.ExpiredSecondaryTokenException;
 import com.aparttime.exception.jwt.InvalidSignatureException;
 import com.aparttime.exception.jwt.InvalidTokenException;
+import com.aparttime.exception.jwt.InvalidTokenTypeException;
 import com.aparttime.exception.jwt.MalformedTokenException;
 import com.aparttime.exception.jwt.UnsupportedTokenException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -43,39 +48,59 @@ public class JwtTokenProvider {
     public String createAccessToken(Long memberId) {
         return createToken(
             memberId,
-            jwtProperties.getAccessTokenExpiration()
+            jwtProperties.getAccessTokenExpiration(),
+            ACCESS_TOKEN
         );
     }
 
     public String createRefreshToken(Long memberId) {
         return createToken(
             memberId,
-            jwtProperties.getRefreshTokenExpiration()
+            jwtProperties.getRefreshTokenExpiration(),
+            REFRESH_TOKEN
+        );
+    }
+
+    public String createSecondaryToken(Long memberId) {
+        return createToken(
+            memberId,
+            jwtProperties.getSecondaryTokenExpiration(),
+            SECONDARY_TOKEN
         );
     }
 
     private String createToken(
         Long memberId,
-        long expiration
+        long expiration,
+        String tokenType
     ) {
         Date now = new Date();
         Date expiry = new Date(now.getTime() + expiration);
 
         return Jwts.builder()
             .setSubject(String.valueOf(memberId))
+            .claim(TOKEN_TYPE, tokenType)
             .setIssuedAt(now)
             .setExpiration(expiry)
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
     }
 
-    public boolean validateAccessToken(String token) {
+    // TODO: validate 로직 리팩터링 필요
+
+    public void validateAccessToken(String token) {
         try {
-            Jwts.parserBuilder()
+            Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
-                .parseClaimsJws(token);
-            return true;
+                .parseClaimsJws(token)
+                .getBody();
+
+            String tokenType = claims.get(TOKEN_TYPE, String.class);
+
+            if (!ACCESS_TOKEN.equals(tokenType)) {
+                throw new InvalidTokenTypeException();
+            }
         } catch (ExpiredJwtException e) {
             throw new ExpiredAccessTokenException();
         } catch (SignatureException e) {
@@ -108,6 +133,34 @@ public class JwtTokenProvider {
         }
     }
 
+    public void validateSecondaryToken(String token) {
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+            String tokenType = claims.get(TOKEN_TYPE, String.class);
+
+            if (!SECONDARY_TOKEN.equals(tokenType)) {
+                throw new InvalidTokenTypeException();
+            }
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredSecondaryTokenException();
+        } catch (SignatureException e) {
+            throw new InvalidSignatureException();
+        } catch (MalformedJwtException e) {
+            throw new MalformedTokenException();
+        } catch (UnsupportedJwtException e) {
+            throw new UnsupportedTokenException();
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidTokenException();
+        }
+
+    }
+
     public String getMemberId(String token) {
         return Jwts.parserBuilder()
             .setSigningKey(key)
@@ -121,6 +174,15 @@ public class JwtTokenProvider {
         String memberId = getMemberId(token);
         UserDetails userDetails = userDetailsService.loadUserByUsername(memberId);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    public String getTokenType(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .get(TOKEN_TYPE, String.class);
     }
 
 }
