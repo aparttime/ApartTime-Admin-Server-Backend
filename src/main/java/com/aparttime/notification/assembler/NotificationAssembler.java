fb01f0f894f00d6fdd232c7ee39da867f761a00d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-12 14:54:22 +0900

    feat: define interface for notification message assembly in `NotificationAssembler`

--- src/main/java/com/aparttime/notification/assembler/NotificationAssembler.java
+++ src/main/java/com/aparttime/notification/assembler/NotificationAssembler.java
@@ -1,5 +1,9 @@
 package com.aparttime.notification.assembler;
 
+import com.aparttime.notification.dto.NotificationMessage;
+import java.util.Map;
+
 public interface NotificationAssembler {
 
+    NotificationMessage assemble(Map<String, Object> data);
 }
