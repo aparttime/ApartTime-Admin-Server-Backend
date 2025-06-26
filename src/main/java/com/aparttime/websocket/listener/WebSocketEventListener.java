c61e354fca199e1f2f789d29f322062a5d1be1b1
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-26 21:19:25 +0900

    feat: extract session metadata on WebSocket connection in `WebSocketEventListener`

--- src/main/java/com/aparttime/websocket/listener/WebSocketEventListener.java
+++ src/main/java/com/aparttime/websocket/listener/WebSocketEventListener.java
@@ -1,4 +1,44 @@
 package com.aparttime.websocket.listener;
 
+import com.aparttime.websocket.principal.StompPrincipal;
+import com.aparttime.websocket.service.WebSocketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class WebSocketEventListener {
+
+    private final WebSocketService sessionService;
+    private final WebSocketService webSocketService;
+
+    @EventListener
+    public void handleConnect(SessionConnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        StompPrincipal principal = (StompPrincipal) accessor.getUser();
+
+        if (principal == null) {
+            log.warn("인증되지 않은 사용자가 WebSocket 연결을 시도했습니다!!");
+            return;
+        }
+
+        String sessionId = accessor.getSessionId();
+        String memberId = principal.getName();
+        String ipAddress = (String) accessor.getSessionAttributes().get("ipAddress");
+
+        log.info(">>> WebSocketEventListener WebSocket Connected: memberId={}, sessionId={}", memberId, sessionId);
+
+        webSocketService.handleSessionConnect(
+            memberId,
+            sessionId,
+            ipAddress
+        );
+    }
+
 }
