6e57789bfdcd0fd96639dad61114f0983a7a2ebe
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-26 21:14:34 +0900

    feat: register custom channel interceptor for inbound WebSocket messages in `WebSocketConfig`

--- src/main/java/com/aparttime/websocket/config/WebSocketConfig.java
+++ src/main/java/com/aparttime/websocket/config/WebSocketConfig.java
@@ -2,9 +2,11 @@ package com.aparttime.websocket.config;
 
 import static com.aparttime.common.constants.WebSocketConstants.*;
 
+import com.aparttime.websocket.interceptor.CustomChannelInterceptor;
 import com.aparttime.websocket.interceptor.CustomHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,6 +18,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final CustomHandshakeInterceptor customHandshakeInterceptor;
+    private final CustomChannelInterceptor customChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(
@@ -32,9 +35,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         MessageBrokerRegistry registry
     ) {
         // 클라이언트에서 구독하는 경로 (서버 -> 클라이언트)
-        registry.enableSimpleBroker(TOPIC_PREFIX);
+        registry.enableSimpleBroker(TOPIC_PREFIX, "/queue");
 
         // 클라이언트가 메시지를 보낼 경로 (클라이언트 -> 서버)
         registry.setApplicationDestinationPrefixes(PUB_PREFIX);
+    }
+
+    @Override
+    public void configureClientInboundChannel(
+        ChannelRegistration registration
+    ) {
+        registration.interceptors(customChannelInterceptor);
     }
 }
