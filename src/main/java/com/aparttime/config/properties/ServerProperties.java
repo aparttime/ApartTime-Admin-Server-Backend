52525c629608868dc4777ed143fc47816d0a6d4e
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-26 21:25:57 +0900

    feat: add configurable server identifier property in `ServerProperties`

--- src/main/java/com/aparttime/config/properties/ServerProperties.java
+++ src/main/java/com/aparttime/config/properties/ServerProperties.java
@@ -1,5 +1,16 @@
 package com.aparttime.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "server")
 public class ServerProperties {
+
+    private String id;
 
 }
