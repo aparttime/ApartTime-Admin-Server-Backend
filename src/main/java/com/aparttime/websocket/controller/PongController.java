9d152335e03da95d1553bfcfe3e1dc799f392714
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-26 21:30:28 +0900

    feat: handle pong message from client and update lastPongTime in `PongController`

--- src/main/java/com/aparttime/websocket/controller/PongController.java
+++ src/main/java/com/aparttime/websocket/controller/PongController.java
@@ -1,5 +1,28 @@
 package com.aparttime.websocket.controller;
 
+import com.aparttime.websocket.principal.StompPrincipal;
+import com.aparttime.websocket.service.WebSocketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class PongController {
+
+    private final WebSocketService webSocketService;
+
+    @MessageMapping("/pong")
+    public void handlePong(
+        StompPrincipal principal
+    ) {
+        String memberId = principal.getName();
+
+        log.info(">>> PongController memberId: {}", memberId);
+
+        webSocketService.updateLastPongTime(memberId);
+    }
 
 }
