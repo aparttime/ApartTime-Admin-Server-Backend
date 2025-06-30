27b2f5985236da7d62f131d043417a7fa80cf631
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-06-30 15:27:38 +0900

    feat: add session registry support in `CustomWebSocketHandler`

--- src/main/java/com/aparttime/websocket/handler/CustomWebSocketHandler.java
+++ src/main/java/com/aparttime/websocket/handler/CustomWebSocketHandler.java
@@ -1,5 +1,38 @@
 package com.aparttime.websocket.handler;
 
-public class CustomWebSocketHandler {
+import com.aparttime.websocket.registry.WebSocketSessionRegistry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomWebSocketHandler implements WebSocketHandlerDecoratorFactory {
+
+    private final WebSocketSessionRegistry sessionRegistry;
+
+    @Override
+    public WebSocketHandler decorate(WebSocketHandler handler) {
+        return new WebSocketHandlerDecorator(handler) {
+            @Override
+            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+                log.info(">>> WebSocket session established: {}", session.getId());
+                sessionRegistry.registerSession(session.getId(), session);
+                super.afterConnectionEstablished(session);
+            }
+
+            @Override
+            public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+                log.info(">>> WebSocket session closed: {}", session.getId());
+                sessionRegistry.removeSession(session.getId());
+                super.afterConnectionClosed(session, closeStatus);
+            }
+        };
+    }
 }
