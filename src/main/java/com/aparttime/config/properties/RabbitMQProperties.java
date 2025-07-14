f767c5ef779265843080e1a52fe16e4d67bbf85b
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-07-14 21:51:11 +0900

    feat: add `RabbitMQProperties`

--- src/main/java/com/aparttime/config/properties/RabbitMQProperties.java
+++ src/main/java/com/aparttime/config/properties/RabbitMQProperties.java
@@ -1,29 +1,4 @@
 package com.aparttime.config.properties;
 
-import lombok.Getter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@Getter
-@ConfigurationProperties(prefix = "spring.rabbitmq")
-public class RabbitMQProperties {
-
-    private final String host;
-    private final int port;
-    private final String username;
-    private final String password;
-    private final String queueName;
-
-    public RabbitMQProperties(
-        String host,
-        int port,
-        String username,
-        String password,
-        String queueName
-    ) {
-        this.host = host;
-        this.port = port;
-        this.username = username;
-        this.password = password;
-        this.queueName = queueName;
-    }
+public record RabbitMQProperties() {
 }
