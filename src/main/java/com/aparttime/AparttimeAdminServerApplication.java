2a841b0021105a7d6e0a34751aeeaf72c944e3b5
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-07-14 11:15:11 +0900

    feat: add @ConfigurationPropertiesScan in `AparttimeAdminServerApplication`

--- src/main/java/com/aparttime/AparttimeAdminServerApplication.java
+++ src/main/java/com/aparttime/AparttimeAdminServerApplication.java
@@ -2,12 +2,14 @@ package com.aparttime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AparttimeAdminServerApplication {
 
     public static void main(String[] args) {
